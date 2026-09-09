af57d1aa71c81af1158641f64fe63555600ee250
MaxMintyTheBigBoss <mealc666@gmail.com>
2026-09-09 10:41:03 +0300

    v0.0.14: Placeholder_5 includes objects + custom_object + ПГРЭЗ checkbox; build

--- app.py
+++ app.py
@@ -62,7 +62,7 @@ from permit_update_gui import UpdateDialog
 
 # Константы
 APP_NAME = "dazvol_u_zonu"
-APP_VERSION = "0.0.13"
+APP_VERSION = "0.0.14"
 APP_EXE_NAME = f"dazvol_u_zonu_ver.{APP_VERSION}.exe"
 BG_COLOR = "#E6EBE0"
 BTN_BG = "#CAD4CC"
