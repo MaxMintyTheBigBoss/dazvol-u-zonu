8d78be1cd1953c4d08853c2cad52b1fab5a4db59
MaxMintyTheBigBoss <MaxMintyTheBigBoss@users.noreply.github.com>
2026-09-15 11:32:50 +0300

    v0.1.18: updated templates (Заявление 14.3, Заявление 14.5, transport permits)

--- app.py
+++ app.py
@@ -64,7 +64,7 @@ from permit_update_gui import UpdateDialog
 
 # Константы
 APP_NAME = "dazvol_u_zonu"
-APP_VERSION = "0.1.17"
+APP_VERSION = "0.1.18"
 APP_EXE_NAME = f"dazvol_u_zonu_ver.{APP_VERSION}.exe"
 
 
